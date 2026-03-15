f3bda664c2e57dded59d8c601702c521e0f6bbf0
Shrutanjaya Bhardwaj <sh.bhardwaj.office@gmail.com>
2026-03-15 21:51:58 +0530

    fix: guard createWindow against activate before app ready on macOS

--- main.js
+++ main.js
@@ -1292,7 +1292,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
+  if (app.isReady() && BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
